a96e90c3e195cc28c95ccdc733572380a1e97207
Veta <veta1027@yandex.ru>
2025-09-24 15:10:38 +0300

    09.24.25

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,74 @@ import './App.scss'
 
 function App() {
   return (
-    <div className='wrapper'>
-      <header>
-      <div className='headerLeft'>
+    <div className='wrapper clear'>
+      <header className='d-flex justify-between align-center'>
+      <div className='d-flex align-center'>
           <img width={60} height={60} src='public\img\logo.png' />
         <div className='headerInfo'>
-          <h3>название магазина</h3>
+          <h3 className='text-uppercase'>NAME SHOPS</h3>
           <p>слоган</p>
         </div>
       </div>
 
-        <ul className='HeaderRight'>
-          <li>
-           <img width={28} height={28} src='public\img\dog.png'/>
+        <ul className='d-flex'>
+          <li className='mr-30'>
+           <img className='iconHerder' width={18} height={18} src='public\img\human.png'/>
           <samp>1205 руб</samp>
           </li>
           <li>
-           <img width={18} height={18} src='public\img\basket.png'/>
+           <img className='iconHerder' width={18} height={18} src='public\img\basket.png'/>
           </li>
         </ul>
  </header>
 
-  <div className='content'>
-    <h1> контент для магазина </h1>
-       <p> 011110011</p>
+  <div className='content p-40'>
+    <h1 className='mb-40'> контент для магазина </h1>
+ 
+
+   <div className='card'>
+    <img width={133} height={133} src="public\img\product.img\content 1.jpg" alt="product" />
+    <h5>нзвание товара</h5>
+    <div className='d-flex justify-between align-center'> 
+      <div className='d-flex flex-column'>
+        <samp>цена:</samp>
+        <b>1 997 pуб.</b>
+        <button className='button'>
+          <img width={11} height={11} src="public\img\+.png" alt="" />
+        </button>
+      </div>
+    </div>
   </div>
 
+  <div className='card'>
+    <img width={133} height={133} src="public\img\product.img\content 2.jpg" alt="product" />
+    <h5>нзвание товара</h5>
+    <div className='d-flex justify-between align-center'> 
+      <div className='d-flex flex-column'>
+        <samp>цена:</samp>
+        <b>1 997 pуб.</b>
+        <button className='button'>
+          <img width={11} height={11} src="public\img\+.png" alt="" />
+        </button>
+      </div>
+    </div>
+  </div>
+
+  <div className='card'>
+    <img width={133} height={133} src="public\img\product.img\content 3.jpg" alt="product" />
+    <h5>нзвание товара</h5>
+    <div className='d-flex justify-between align-center'> 
+      <div className='d-flex flex-column'>
+        <samp>цена:</samp>
+        <b>1 997 pуб.</b>
+        <button className='button'>
+          <img width={11} height={11} src="public\img\+.png" alt="" />
+        </button>
+      </div>
+    </div>
+  </div>
+   
+ </div>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.scss'
-import App from './App.jsx'
+import App from './App'
+import './index.scss';
+import 'macro-css';
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
